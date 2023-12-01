c2d41da674a1cec9f678f4fdd7f1657e10fbae97
Diksha Upreti <dikshaupreti2012@gmail.com>
2023-12-01 17:04:42 +0530

    changes regarding episode 3

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 /**
  * < div id= "parent">
  *      <div id= "child1">
@@ -11,15 +13,46 @@
  * </div>
  * 
  */
-let heading = React.createElement('h1', {id: "heading"}, [
-    React.createElement('div', {id: 'child1'}, [
-        React.createElement('h1', {}, 'Hii daddy'),
-        React.createElement('h2', {}, 'Hiiiiiiiii'),
-    ]),
-    React.createElement('div', {id: 'child2'}, [
-        React.createElement('h1', {}, 'Hii chhiiii'),
-        React.createElement('h2', {}, 'Hiiiiiiiii'),
-    ])
-])
+// let heading1 = React.createElement('h1', {id: "heading"}, [
+//     React.createElement('div', {id: 'child1'}, [
+//         React.createElement('h1', {}, 'Hii daddy'),
+//         React.createElement('h2', {}, 'Hiiiiiiiii'),
+//     ]),
+//     React.createElement('div', {id: 'child2'}, [
+//         React.createElement('h1', {}, 'Hii chhiiii'),
+//         React.createElement('h2', {}, 'Hiiiiiiiii'),
+//     ])
+// ])
+//console.log(heading1)
+// component inside react element
+let heading2 = (<div>
+    <div>
+        <h1>hiiii</h1>
+        <Section />
+        <Section></Section>
+        {   /** as compoent are basically function only */
+            Section()}
+    </div>
+</div>)
+console.log(heading2)
+// other way of writing component
+const Section = function() {
+    <div>
+        <h1>diksha</h1>
+    </div>
+}
+    
+const num = 1000
+const Footer = () => {
+    return (
+        <div>
+    <div>
+        <h1>hiiii</h1>
+        {heading2} {num + 2000} 
+        <Section />
+    </div>
+    </div>
+    )
+}
 let root= ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(<Footer />)
